54e78fbe7a207876784451688c904031dd60c3cc
dilmurod <dilmurod121202@gmail.com>
2024-01-25 00:51:22 +0500

    fix: icons name

--- components/inputCard/index.vue
+++ components/inputCard/index.vue
@@ -1,17 +1,17 @@
 <template>
     <form class="input container mx-auto">
         <div class="input__box">
-            <img src="@/assets/images/search.svg" alt="" class="input__search-icon">
+            <img src="/icons/search.svg" alt="" class="input__search-icon">
             <input type="search" class="input__search focus:ring-0 active:ring-0" placeholder="Search by: Job tittle, Position, Keyword...">
         </div>
 
         <div class="input__box">
-            <img class="input__search-location" src="@/assets/images/MapPinIcon.svg" alt="">
+            <img class="input__search-location" src="/icons/MapPinIcon.svg" alt="">
             <input type="search" class="input__search" placeholder="Manzilni Kiriting">
-            <img src="@/assets/images/crosshair.svg" alt="" class="input__crosshair">
+            <img src="/icons/BellRinging.svg" alt="" class="input__crosshair">
             <div class="input__btn-group">
                 <button class="input__btn0">
-                    <img src="@/assets/images/filter.svg" alt="" class="input__btn-icon">
+                    <img src="/icons/filter.svg" alt="" class="input__btn-icon">
                     Filters
                 </button>
                 <button class="input__btn1">Qidirish</button>
